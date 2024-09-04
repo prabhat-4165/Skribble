8e125d36a9e29ddfc872ecb5a59607a3230c2ebf
prabhat-4165 <prabhu27082001@gmail.com>
2024-09-04 15:40:57 +0530

    deleted

--- server/server.js
+++
@@ -1,7 +0,0 @@
-import  {connectDB}  from "./data/database.js";
-import {server} from "./index.js"
-connectDB();
-
-server.listen(process.env.PORT,()=>{ 
-    console.log("server is working")
-}) 
--- server/util/SecretToken.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import jwt from "jsonwebtoken";
-
-export const createSecretToken = (id) => {
-  return jwt.sign({ id }, process.env.TOKEN_KEY, {
-    expiresIn: 3 * 24 * 60 * 60,
-  });
-};
